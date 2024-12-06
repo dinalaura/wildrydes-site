6eb057b6cafc3ae214961e1afcb3fe43856f1333
Dina Laura <64177544+dinalaura@users.noreply.github.com>
2024-12-05 23:34:34 -0600

    3cognito

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_d56UrmgcG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '54hmbpn4ct7tic8f6q8qbvft2u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_Zeu3ev2GJ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '119qp5o0cgqjn6145o9nau9fla', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
